e9c4987f41c2bf5e7a686b2ef08d8c0fc6cb4560
Cristiano Assis <cristiano.aa@gmail.com>
2026-01-11 20:31:34 -0300

    First commit

---
+++ src/main/java/com/algaworks/algasensors/device/management/DataClass.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.device.management;
+
+public class DataClass {
+
+    private String a, b, c;
+
+    public DataClass(String a, String b, String c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+}
