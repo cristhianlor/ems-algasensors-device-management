9250db3f796037d1f935b90889bcc47f14a2a1a9
Cristiano Assis <cristiano.aa@gmail.com>
2026-05-03 17:58:57 -0300

    Implement Sensor entity and repository; update SensorController to use SensorId

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/config/jpa/TSIDToLongJPAAtributeConverter.java
@@ -0,0 +1,19 @@
+package com.algaworks.algasensors.device.management.api.config.jpa;
+
+import io.hypersistence.tsid.TSID;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class TSIDToLongJPAAtributeConverter implements AttributeConverter<TSID, Long> {
+
+    @Override
+    public Long convertToDatabaseColumn(TSID attribute) {
+        return attribute.toLong();
+    }
+
+    @Override
+    public io.hypersistence.tsid.TSID convertToEntityAttribute(Long dbData) {
+        return TSID.from(dbData);
+    }
+}
